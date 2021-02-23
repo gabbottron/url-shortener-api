231234dc11b2b2ee75c2a7fdfa2241387decc4dc
Geoffrey Abbott <geoffreyabbott@ramrod.local>
2021-02-23 11:32:31 -0500

    devise changes to support API mode

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
+  # These are the default enabled settings for Devise User,
+  # I am disabling them for API mode
+  #devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :validatable
+
+  # This must be included for the password checking 
+  # that devise does -gabbott
+  devise :database_authenticatable
 end
